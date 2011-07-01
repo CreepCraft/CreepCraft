eeb1f0d919bf33691bc68a3065433aae3dee110c
Max Henkes <henkesmax@gmail.com>
2011-07-01 21:40:49 +0200

    Added permissions

--- src/net/creepcraft/CreepCraft.java
+++ src/net/creepcraft/CreepCraft.java
@@ -4,13 +4,18 @@ import java.util.logging.Logger;
 
 import org.bukkit.command.*;
 import org.bukkit.event.Event;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import com.nijiko.permissions.PermissionHandler;
+import com.nijikokun.bukkit.Permissions.Permissions;
 
 public class CreepCraft extends JavaPlugin {
 	private final CreepCraftPlayerListener playerListener = new CreepCraftPlayerListener(
 			this);
 	public static final Logger log = Logger.getLogger("Minecraft");
+	public static PermissionHandler permissionHandler;
 
 	@Override
 	public void onDisable() {
@@ -25,6 +30,7 @@ public class CreepCraft extends JavaPlugin {
 		PluginManager pm = getServer().getPluginManager();
 		pm.registerEvent(Event.Type.PLAYER_JOIN, playerListener,
 				Event.Priority.Highest, this);
+		setupPermissions();
 	}
 
 	@Override
@@ -35,5 +41,24 @@ public class CreepCraft extends JavaPlugin {
 		}
 		return false;
 	}
+
+	private void setupPermissions() {
+	    if (permissionHandler != null) {
+	        return;
+	    }
+	    
+	    Plugin permissionsPlugin = this.getServer().getPluginManager().getPlugin("Permissions");
+	    
+	    if (permissionsPlugin == null) {
+	        log.info("Permission system not detected, defaulting to OP");
+	        return;
+	    }
+	    
+	    permissionHandler = ((Permissions) permissionsPlugin).getHandler();
+	    log.info("Found and will use plugin "+((Permissions)permissionsPlugin).getDescription().getFullName());
+	}
+
+
+
 
 }
